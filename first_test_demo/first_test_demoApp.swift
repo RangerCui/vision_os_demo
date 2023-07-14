0546c3446bb71731abfbbb9c6ff2c5861e4a1141
hubery.cui <ranger_cui@163.com>
2023-07-15 02:23:23 +0800

    init vision os demo

---
+++ first_test_demo/first_test_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_test_demoApp.swift
+//  first_test_demo
+//
+//  Created by Ranger on 2023/7/15.
+//
+
+import SwiftUI
+
+@main
+struct first_test_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
